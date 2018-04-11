2ff1a0b4fe75bb9ea97dc96e401f5da36a919111
dominykasbrezgys <d.brezgys@gmail.com>
2018-04-11 17:55:16 +0100

    Modified AcademicStaff Modules page
    - Added dropdown menu option to 'Modules' navbar item.
    - All modules are now visible to 'AcademicStaff'

--- controllers/academicstaff/modules.js
+++ controllers/academicstaff/modules.js
@@ -4,12 +4,33 @@ CONTROLLER
 var express = require('express')
     ,router = express.Router()
     ,AcademicStaff = require('../../models/academicstaff')
+    ,Module = require('../../models/module')
     ,Student = require('../../models/student');
 
 
-router.get('/academicstaff/modules', function(req, res) {
+router.get('/academicstaff/allmodules', function(req, res) {
 	username = req.session.user;
-	res.render("academicstaff_modules");
+	rows = []
+
+	//Get all modules
+	Module.getAll(function(modules){
+		//Retrieve teaching/assessing information associated with each module
+		modules.forEach(function(module){
+			Module.getTeachingStaff(module['ModuleCode'], function(teaching){
+				Module.getAssessingStaff(module['ModuleCode'], function(assessing){
+					rows.push({
+						ModuleCode : module['ModuleCode'],
+						ModuleName : module['ModuleName'],
+						TeachingStaff : teaching,
+						AssessingStaff : assessing
+					});
+					if (rows.length == modules.length){
+						res.render("academicstaff_all_modules",{rows:rows})
+					}
+				});
+			});
+		});
+	});
 });
 
 module.exports = router;
--- models/module.js
+++ models/module.js
@@ -17,3 +17,36 @@ exports.getByModuleCode = function(moduleCode, callback) {
 		callback(result[0]);
     });
 };
+
+exports.getAll = function(callback) {
+	sql = "SELECT * FROM Module";
+	con.query(sql, function(err, result) {
+		if (err) throw err;
+		callback(result);
+    });
+};
+
+exports.getTeachingStaff = function(moduleCode, callback) {
+	sql = "SELECT AcademicStaff.FirstName, AcademicStaff.LastName"+
+		" FROM AcademicStaff"+
+		" INNER JOIN Teaching"+
+		" ON AcademicStaff.id = Teaching.AcademicStaffID"+
+		" WHERE Teaching.ModuleCode= '"+moduleCode+"'";
+	con.query(sql, function(err, result) {
+		if (err) throw err;
+		callback(result);
+    });
+};
+
+exports.getAssessingStaff = function(moduleCode, callback) {
+	sql = "SELECT AcademicStaff.FirstName, AcademicStaff.LastName"+
+		" FROM AcademicStaff"+
+		" INNER JOIN Assessing"+
+		" ON AcademicStaff.id = Assessing.AcademicStaffID"+
+		" WHERE Assessing.ModuleCode= '"+moduleCode+"'";
+	con.query(sql, function(err, result) {
+		if (err) throw err;
+		callback(result);
+    });
+};
+
